54a59f246de24ad89360e432aecb8840890302ac
kristiansnts <epafroditus.kristian@gmail.com>
2021-02-22 13:05:27 +0700

    applying style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,17 @@ module.exports = {
     extend: {
     	colors: {
     		primary: {
-    			
+    			darkCyan: 'hsl(185, 75%, 39%)',
+    			desatBlue: 'hsl(229, 23%, 23%)',
+    			darkBlue: 'hsl(227, 10%, 46%)'
+    		},
+    		netral: {
+    			darkGray: 'hsl(0, 0%, 59%)'
     		}
-    	}
+    	},
+    	fontFamily: {
+    		sans: ['Kumbh Sans', 'sans-serif'],
+    	}	
     },
   },
   variants: {
